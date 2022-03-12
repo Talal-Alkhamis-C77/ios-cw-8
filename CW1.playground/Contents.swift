27deb02cdcbffffe21030700e8c0270ffd486bdc
Talal-Alkhamis-C77 <70804941+Talal-Alkhamis-C77@users.noreply.github.com>
2022-03-12 10:58:59 +0300

    meow

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -3,3 +3,19 @@ import UIKit
 // التمرين الأول
 // ClassWork 1
 
+let name = "Meow"
+
+var birthYear = 2005
+
+var age = 2022 - birthYear
+
+print("Hello my name is \(name), and my age is \(age)")
+
+
+var one = "8"
+
+var two = 4
+
+var three = 2.5
+
+three = (Double(one) ?? 0) + (Double(two) ?? 0)
